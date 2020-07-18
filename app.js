0174c5de060b0ae9982b2cd5ab5c4b519a7d4b4f
duongptgch18218 <duongptgch18218@fpt.edu.vn>
2020-07-18 08:37:45 +0700

    upload

---
+++ app.js
@@ -0,0 +1,93 @@
+const path = require('path');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const session = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(session);
+const csrf = require('csurf');
+const flash = require('connect-flash');
+
+const cors = require('cors')
+
+const errorController = require('./controllers/error');
+const User = require('./models/user');
+
+const MONGODB_URI =
+  'mongodb+srv://ryu:201085@mongodb.kwnvj.mongodb.net/test';
+
+const app = express();
+const store = new MongoDBStore({
+  uri: MONGODB_URI,
+  collection: 'sessions'
+});
+const csrfProtection = csrf();
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+const authRoutes = require('./routes/auth');
+
+app.use(cors())
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(
+  session({
+    secret: 'my secret',
+    resave: false,
+    saveUninitialized: false,
+    store: store
+  })
+);
+app.use(csrfProtection);
+app.use(flash());
+
+app.use((req, res, next) => {
+  const rawHeader = req.headers
+  console.log(res.statusCode)
+  // console.log(req.ip)
+  // console.log(req._parsedOriginalUrl)
+  const from = rawHeader.referer
+  // console.log(from)
+  console.log(req)
+  const protocol = req.protocol
+  const to = protocol + "://" + rawHeader.host + req.url;
+  // console.log(to)
+  const brower = rawHeader.connection
+  next();
+})
+
+app.use((req, res, next) => {
+  if (!req.session.user) {
+    return next();
+  }
+  User.findById(req.session.user._id)
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(err => console.log(err));
+});
+
+app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.session.isLoggedIn;
+  res.locals.csrfToken = req.csrfToken();
+  next();
+});
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+app.use(authRoutes);
+
+app.use(errorController.get404);
+
+mongoose
+  .connect(MONGODB_URI)
+  .then(result => {
+    app.listen(process.env.PORT || 3000);
+  })
+  .catch(err => {
+    console.log(err);
+  });
